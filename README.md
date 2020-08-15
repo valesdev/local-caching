61b77ae688664ba9fb4c47afdef408d091d57779
Chon <ychongsaytc@gmail.com>
2020-08-15 11:09:21 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# local-caching
+
+[![Version](https://img.shields.io/npm/v/local-caching.svg)](https://www.npmjs.com/package/local-caching)
+[![Downloads](https://img.shields.io/npm/dm/local-caching.svg)](https://npmcharts.com/compare/local-caching?minimal=true)
+[![License](https://img.shields.io/npm/l/local-caching.svg)](https://www.npmjs.com/package/local-caching)
+
+A programmatic data caching wrapper for browsers, based on localStorage.
+
+## Installation
+
+```sh
+npm install --save local-caching
+```
+
+## Usage
+
+### Instance
+
+```js
+import LocalCaching from 'local-caching'
+
+const localCaching = new LocalCaching()
+localCaching.init()
+```
+
+### Set cache data
+
+```js
+localCaching.set('foo', 'bar', 300e3) // 5 minues
+localCaching.set('numberValue', 12138, 300e3)
+localCaching.set('booleanValue', true, 300e3)
+localCaching.set('nullValue', null, 300e3)
+localCaching.set('arrayValue', ['bar', 12138, null, true], 300e3)
+localCaching.set('objectValue', { foo: 'bar' }, 300e3)
+```
+
+### Get cache data
+
+```js
+const foo          = localCaching.get('foo')
+const numberValue  = localCaching.get('numberValue')
+const booleanValue = localCaching.get('booleanValue')
+const nullValue    = localCaching.get('nullValue')
+const arrayValue   = localCaching.get('arrayValue')
+const objectValue  = localCaching.get('objectValue')
+```
+
+### Flush all items
+
+```js
+localCaching.flushAll()
+```
+
+### Flush only expired items
+
+```js
+localCaching.flushExpired()
+```
+
+## Constructor
+
+```js
+new LocalCaching(options)
+```
+
+| Option | Type | Default | Description |
+| --- | --- | --- | --- |
+| `storeKeyPrefix` | string | `'cache::'` | Key prefix for cache items. |
+| `storeMetaKeyPrefix` | string | `'cachemeta::'` | Key prefix for cache meta items. |
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,81 @@
+export default class {
+
+  constructor ({ storeKeyPrefix = 'cache::', storeMetaKeyPrefix = 'cachemeta::' } = { storeKeyPrefix: 'cache::', storeMetaKeyPrefix: 'cachemeta::' }) {
+    this.storeKeyPrefix = storeKeyPrefix
+    this.storeMetaKeyPrefix = storeMetaKeyPrefix
+  }
+
+  get isSupported () {
+    return 'localStorage' in window
+  }
+
+  init () {
+    if (!this.isSupported) {
+      throw new Error('Your browser does not support localStorage.')
+    }
+
+    this.flushExpired()
+  }
+
+  set (key, value, expiry) {
+    this._set(`${this.storeKeyPrefix}${key}`, value)
+    this._set(`${this.storeMetaKeyPrefix}${key}`, { exp: +new Date() + expiry })
+    this.flushIfNeeded(key)
+  }
+
+  get (key) {
+    this.flushIfNeeded(key)
+    return this._get(`${this.storeKeyPrefix}${key}`)
+  }
+
+  remove (key) {
+    this._remove(`${this.storeKeyPrefix}${key}`)
+    this._remove(`${this.storeMetaKeyPrefix}${key}`)
+  }
+
+  flushAll () {
+    this._getKeys().forEach(key => {
+      this.remove(key)
+    })
+  }
+
+  flushExpired () {
+    this._getKeys().forEach(key => {
+      this.flushIfNeeded(key)
+    })
+  }
+
+  flushIfNeeded (key) {
+    const meta = this._get(`${this.storeMetaKeyPrefix}${key}`)
+    if (typeof meta === 'object' && meta !== null && meta.exp < +new Date()) {
+      this.remove(key)
+    }
+  }
+
+  _getKeys () {
+    const keys = []
+    Object.keys(window.localStorage).forEach(key => {
+      if (key.indexOf(this.storeKeyPrefix) === 0) {
+        keys.push(key)
+      }
+    })
+    return keys
+  }
+
+  _set (key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  _get (key) {
+    const payload = window.localStorage.getItem(key)
+    if (typeof payload === 'string' && payload.length > 0) {
+      return JSON.parse(payload)
+    }
+    return null
+  }
+
+  _remove (key) {
+    window.localStorage.removeItem(key)
+  }
+
+}
